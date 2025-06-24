b094db3e3a680e58f215ee3a5473d53657813019
Akshay Arora <akshayaurora@gmail.com>
2025-06-24 13:10:27 +0530

    Update setup.py

--- bindings/python/setup.py
+++ bindings/python/setup.py
@@ -1,5 +1,6 @@
 from os.path import dirname, join, abspath
 from setuptools import setup, Extension
+from Cython.Build import cythonize
 
 root_dir = abspath(dirname(__file__))
 
@@ -14,6 +15,18 @@ except ImportError:
 with open(join(root_dir, '..', '..', 'VERSION')) as f:
     __version__ = f.readline()
 
+extensions = Extension(
+        name = 'evolution_benchmark',
+        sources = [join(root_dir, source_file)],
+        language='c',
+        include_dirs=[join(root_dir, '..', '..', 'lib', 'inc')],
+        libraries=['evolution-benchmark'],
+        library_dirs=[
+            join(root_dir, '..', '..', 'build_cmake', 'lib'),
+            join(root_dir, '..', '..', 'build_cmake', 'lib', 'Release'),
+        ])
+
+cython_directives = {'language_level': "3"}
 setup(
     name='evolution_benchmark',
     description='Python Interface to Waverian Evolution Benchmark',
@@ -25,20 +38,6 @@ setup(
     options={
         'bdist_wheel':{'universal':'1'}
     },
-    ext_modules=[Extension(
-        name = 'evolution_benchmark',
-        sources = [join(root_dir, source_file)],
-        language='c',
-        include_dirs=[join(root_dir, '..', '..', 'lib', 'inc')],
-        libraries=['evolution-benchmark'],
-        library_dirs=[
-            join(root_dir, '..', '..', 'build_cmake', 'lib'),
-            join(root_dir, '..', '..', 'build_cmake', 'lib', 'Release'),
-        ],
-        language_level='3',
-        compiler_directives={
-            'language_level': 3
-        },
-    )],
+    ext_modules= cythonize(extensions, compiler_directives=cython_directives),
     version=__version__
 )
